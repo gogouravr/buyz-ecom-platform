e9e927a9fc41c2be264f53e04ffb3e5763ba9c72
Gourav Dev <gouravr244@gmail.com>
2023-09-12 00:54:45 +0530

    Change module type to  from

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'
+import path from 'path'
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+app.get('/hello',(req,res)=>{
+     res.json({msg: 'hello world!'});
+});
 
 
 app.listen(8080, () => {
