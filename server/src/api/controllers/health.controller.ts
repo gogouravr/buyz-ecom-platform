89f8abc2e07de5d9c6d5dfd8a3956b42efacf693
gouravrajbit <gouravr244@gmail.com>
2023-09-17 23:33:03 +0530

    Add `es-lint` to the project

--- server/src/api/controllers/health.controller.ts
+++ server/src/api/controllers/health.controller.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response, Router } from 'express';
+import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/health', (req: Request, res: Response) => {
     res.json({ status: 'OK!' });
-})
+});
 
 export const healthRouter = router;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { healthRouter } from './api/controllers/health.controller';
 
 const app = express();
@@ -6,7 +6,7 @@ const port = 3000;
 
 const v1_BASE_PATH = '/api/v1';
 
-app.use(v1_BASE_PATH, healthRouter);  
+app.use(v1_BASE_PATH, healthRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
