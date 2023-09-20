8c5a9faf7e8641c08f4a32a7f04e36ec29566eff
gouravrajbit <gouravr244@gmail.com>
2023-09-21 00:08:28 +0530

    Add logger file

---
+++ server/src/utils/logger.ts
@@ -0,0 +1,18 @@
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+    level: 'info', // Change the log level as needed
+    format: format.combine(
+        format.colorize(),
+        format.timestamp(),
+        format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level}]: ${message}`;
+        })
+    ),
+    transports: [
+        new transports.Console(),
+        // Add other transports here (e.g., file transport)
+    ],
+});
+
+export default logger;
