39fda9ea6df70e301e1b5df3003f42947987b9af
gouravrajbit <gouravr244@gmail.com>
2023-09-27 23:50:57 +0530

    Add `shoppers` table

--- server/database/vendor/vendor.products.sql
+++ server/database/vendor/vendor.products.sql
@@ -7,5 +7,7 @@ CREATE TABLE vendor.products (
     price DECIMAL(10, 2) NOT NULL,
     stock_quantity INT NOT NULL,
     category VARCHAR(50),
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY (product_id, variant_id)
 );
