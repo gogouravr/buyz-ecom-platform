91a9f2e9e346c2dbe6d637b2e1feb261b1c1300b
gouravrajbit <gouravr244@gmail.com>
2023-09-30 23:56:57 +0530

    Alter `shopper` table

--- server/database/shopper/shopper.shoppers.sql
+++ server/database/shopper/shopper.shoppers.sql
@@ -1,11 +1,13 @@
 CREATE TABLE shopper.shoppers (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INTEGER,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone_number VARCHAR(15),
     accepts_email_marketing BOOLEAN,
     accepts_sms_marketing BOOLEAN,
+    country VARCHAR(50),
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
