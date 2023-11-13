93f26fc6e88b6874bdf94731fd6033df310383e8
gouravrajbit <gouravr244@gmail.com>
2023-11-13 20:27:15 +0530

    Refernce models for db

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,8 @@
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 import dotenv from 'dotenv';
 
+import * as models from './models';
+
 dotenv.config();
 
 const { DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT } = process.env;
@@ -11,6 +13,10 @@ const sequelizeOptions: SequelizeOptions = {
     password: DATABASE_PASSWORD,
     host: DATABASE_HOST,
     port: Number(DATABASE_PORT), // Replace with your PostgreSQL port if necessary
+    models: Object.values(models),
+    define: {
+        timestamps: false, // Disable timestamps for all models
+    },
     dialect: 'postgres', // Specify the database dialect
     logging: true, // Set to true to enable logging
 };
