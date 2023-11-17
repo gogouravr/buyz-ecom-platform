65d903a27495632cfb9ebcc318bb699e548a395d
gouravrajbit <gouravr244@gmail.com>
2023-11-18 00:21:51 +0530

    Add get all products controller

--- src/api/controllers/product.controller.ts
+++ src/api/controllers/product.controller.ts
@@ -8,11 +8,22 @@ const product = express.Router();
 product.post('/product', async (req: Request, res: Response) => {
     try {
         const retData = await productManager.addProduct(req.body as Product);
-        res.json(retData);
+        return res.json(retData);
     } catch (error) {
         console.log(error);
         res.status(400).json({ status: 400, msg: 'Bad request body' });
     }
 });
+
+product.get('/product/all', async (req: Request, res: Response) => {
+    try {
+        const products = await productManager.getAllProducts();
+        return res.json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 400, msg: 'Something went wrong' });
+    }
+});
+
 
 export const productRouter = product;
--- src/api/managers/product.manager.ts
+++ src/api/managers/product.manager.ts
@@ -3,11 +3,20 @@ import { Product } from '../../models';
 class ProductManager {
     /**
      * Method to add product to the DB
-     * @param productData - Product data
+     * @param - productData  Product data
      * @returns Inserted product record
      */
     async addProduct(productData: any): Promise<Product | null> {
         return await Product.create(productData);
+    }
+
+    /**
+     * Method to return all of the products
+     * @returns all products
+     * @todo Paginate the API
+     */
+    async getAllProducts(): Promise<Product[] | null> {
+        return await Product.findAll();
     }
 }
 
