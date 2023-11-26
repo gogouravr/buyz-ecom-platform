9a169f1a1e52fdbd4ea57ec5dd292bf3fd2b9a17
gouravrajbit <gouravr244@gmail.com>
2023-11-26 23:05:29 +0530

    Folder restructure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Error from './pages/Error';
+import Home from './pages/Home/Home';
+import Error from './pages/Error/Error';
 
 function App() {
   const router = createBrowserRouter([
