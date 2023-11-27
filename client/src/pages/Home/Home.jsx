956caac5578a9f4758d4399e5ea7336a8da79fd8
gouravrajbit <gouravr244@gmail.com>
2023-11-27 23:55:10 +0530

    Add react-router's outlet

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <header>
         <Navbar />
       </header>
+      <Outlet />
     </div>
   );
 }
