6ff567db80d05583279392517fd08a324f407136
gouravrajbit <gouravr244@gmail.com>
2023-11-27 23:54:38 +0530

    Fix linting

--- client/src/components/globalSearch/GlobalSearch.jsx
+++ client/src/components/globalSearch/GlobalSearch.jsx
@@ -9,7 +9,6 @@ export default function GlobalSearch() {
   return (
     <form className="global-search">
       <input aria-label="Search products" className="global-search__input" type="text" />
-      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label  */}
       <button type="submit">
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
