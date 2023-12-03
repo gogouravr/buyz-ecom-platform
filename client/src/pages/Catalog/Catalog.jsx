724b0869223bdbf997d89521d941a05f069e736c
gouravrajbit <gouravr244@gmail.com>
2023-12-03 23:58:17 +0530

    Fetch all the products

--- client/src/pages/Catalog/Catalog.jsx
+++ client/src/pages/Catalog/Catalog.jsx
@@ -1,8 +1,21 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Catalog.scss';
 
 export default function Catalog() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios({
+          method: 'post',
+          url: '/api/v1/product/all'
+        });
+        console.log(res);
+      } catch (err) {}
+    };
+    fetchProducts();
+  }, []);
+
   return <div>This is a product catalog page</div>;
 }
